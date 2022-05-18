29fc1457cf496a23c4667c68f05a735e4690557d
norihide-yamanoi <akaitainoeyamayama@icloud.com>
2022-05-18 22:30:38 +0900

    アプリの国際化とタイムゾーンの設定

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module ManyohExam
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     config.generators do |g|
       g.test_framework :rspec,
                        model_specs: true,
@@ -19,7 +20,7 @@ module ManyohExam
                        routing_specs: false,
                        controller_specs: false,
                        request_specs: false
-    end                   
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
