80df66c45479b74b712aff3dbb0586fbf0cc2be8
norihide-yamanoi <akaitainoeyamayama@icloud.com>
2022-05-19 00:58:06 +0900

    一覧を降順表示にのテスト設定

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -9,14 +9,12 @@ describe 'タスクモデル機能', type: :model do
     end
       context 'タスクの詳細が空の場合' do
       it 'バリデーションにひっかかる' do
-        # ここに内容を記載する
         task = Task.new(name: 'aaa', detail: '')
         expect(task).not_to be_valid
       end
     end
     context 'タスクのタイトルと詳細に内容が記載されている場合' do
       it 'バリデーションが通る' do
-        # ここに内容を記載する
         task = FactoryBot.create(:task)
         expect(task).to be_valid
       end
