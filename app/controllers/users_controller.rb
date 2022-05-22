b70e39269a3e2c9896bc5fffc639a3569c329b53
norihide-yamanoi <akaitainoeyamayama@icloud.com>
2022-05-23 01:12:57 +0900

    ログイン時の遷移や、ログアウト時の動きの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,33 @@
 class UsersController < ApplicationController
+  skip_before_action :login_required, only: [:new, :create]
+
   def new
-    @user = User.new
+    if logged_in?
+      flash[:notice] = '新規登録はログアウトしてからに‼︎'
+      redirect_to tasks_path
+    else
+      @user = User.new
+    end
   end
 
   def create
     @user = User.new(user_params)
-
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = 'ログインしました'
+      redirect_to user_path(@user.id)
+    else
+      render :new
+    end
   end
 
   def show
     @user = User.find(params[:id])
+    @tasks = @user.tasks
+    if @user.id != current_user.id
+      flash[:notice] = 'このページにはアクセスできません‼︎'
+      redirect_to tasks_path
+    end
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
+  resources :sessions
   resources :users
   resources :tasks do
     collection do
